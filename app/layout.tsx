7e0b5c40a252b122ea8fd90daca84889c2691c3d
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2026-04-18 11:59:24 +0300

    Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en" className="dark">
             <body>{children}</body>
+            <Analytics />
         </html>
     );
 }
